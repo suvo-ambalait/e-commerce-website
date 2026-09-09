2567d47666eacf219e0eb8dd3440def571cb2176
suvo <suvo.ambalait@gmail.com>
2026-09-09 09:55:41 +0600

    Add Vendor and Users model with relationships and fillable attributes

--- ecommerce-backend/app/Models/User.php
+++ ecommerce-backend/app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function vendors()
+    {
+        return $this->hasMany(Vendor::class);
+    }
 }
--- ecommerce-backend/app/Models/Vendor.php
+++ ecommerce-backend/app/Models/Vendor.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'phone_number',
+        'nid_number',
+        'nid_front_image',
+        'nid_back_image',
+        'trade_license_number',
+        'trade_license_image',
+        'bank_account_number',
+        'bank_name',
+        'bank_branch',
+        'reject_reason',
+        'approved_at',
+        'rejected_at',
+        'rejected_by',
+        'approved_by',
+        'status',
+    ];
+
+     protected $casts = [
+        'approved_at' => 'datetime',
+        'rejected_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
