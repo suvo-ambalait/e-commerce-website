fcf9da8e926e5f499fc37c990795441e29a4105b
suvo <suvo.ambalait@gmail.com>
2026-09-15 15:10:49 +0600

    feat(controller): add controllers for Cart, Cupon, Order, Payment, Product, ProductBrand, ProductCategory, Review, ShippingAddress, Shop, StockMovement, StoreSetting, Vendor, and Wishlist feat(model): create StockMovement and StoreSetting models feat(migration): add migrations for stock_movements and store_settings tables

---
+++ ecommerce-backend/app/Http/Controllers/Api/V1/ProductCategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductCategory $productCategory)
+    {
+        //
+    }
+}
---
+++ ecommerce-backend/app/Http/Controllers/Api/V1/StoreSettingController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\StoreSetting;
+use Illuminate\Http\Request;
+
+class StoreSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StoreSetting $storeSetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StoreSetting $storeSetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StoreSetting $storeSetting)
+    {
+        //
+    }
+}
